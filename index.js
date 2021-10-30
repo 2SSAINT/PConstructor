9f9eb5acf3e46a947fdd132f16f8d6852d03bcea
2SSAINT <92362081+2SSAINT@users.noreply.github.com>
2021-10-30 23:38:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,42 +2,52 @@ var con = require('./access.json')
 
 // Модуль для сравнения комплектующих
 module.exports = {
-  cpu : function( m, c, r) {
+  cpu_mother : function(c, m) {
     //проц с материнкойcon.catalog[1].models[c].socket
     if(con.catalog[1].models[c].socket != con.catalog[0].models[m].socket)
-    console.log('Сокет материнской платы не соответствует сокету процессора');
-    // проц с ОЗУ
-    if (con.catalog[1].models[c].mem_type != con.catalog[1].models[r].type)
-    console.log('Тип оперативной памяти не поддерживается процессором');
+    return 'Сокет материнской платы не соответствует сокету процессора';
   },
-  ram : function(r, m) {
+    // проц с ОЗУ
+  cpu_ram : function(c, r) {
+    if (con.catalog[1].models[c].mem_type != con.catalog[1].models[r].type)
+    return'Тип оперативной памяти не поддерживается процессором';
+  },
+  ram_mother : function(r, m) {
     // ОЗУ и мать
     if (con.catalog[2].models[r].type != con.catalog[0].models[m].ram_type)
-    console.log('Тип оперативной памяти не поддерживается материнской платой');
+    return'Тип оперативной памяти не поддерживается материнской платой';
   },
-  graphics_card : function(g, m, c) {
-    //видеокарта и мать НЕДОДЕЛАНО
-    if (con.catalog[0].models[m].pcie_x8 != 0 && con.catalog[3].models[g].interface != 'PCI-E 8x')
-    console.log('Материнская плата и видеокарта несовместимы по интерфейсу');
-    else if (con.catalog[0].models[m].pcie_x4 != 0 && con.catalog[3].models[g].interface != 'PCI-E 4x')
-    console.log('Материнская плата и видеокарта несовместимы по интерфейсу');
-    else if (con.catalog[0].models[m].pcie_x2 != 0 && con.catalog[3].models[g].interface != 'PCI-E 2x')
-    console.log('Материнская плата и видеокарта несовместимы по интерфейсу');
-    else if (con.catalog[0].models[m].pcie_x2 != 0 && con.catalog[3].models[g].interface != 'PCI-E 1x')
-    console.log('Материнская плата и видеокарта несовместимы по интерфейсу');
+  graphicsCard_mother : function(g, m) {
+    if (con.catalog[0].models[m].pcie_x16 == 0)
+    return'На метеринсую плату нельзя поставить видеокарту';
   },
-  cooler : function (col, cpu) {
+  graphicsCard_case: function(g, c) {
+    if (con.catalog[3].models[g].lenght > con.catalog[8].models[c].max_lenght_graph_card)
+    return 'Длина видеокарты превышает допустимую корпусом'
+  },
+  cooler_cpu : function (col, cpu) {
     // кулер и процессор
+    let str1 = "",str2 = "";
     var cp = 0;
-    for (var i = 0; i < con.catalog[4].models[col].socket.lenght; i++) {
-      if (con.catalog[4].models[col].socket[i] == con.catalog[1].models[cpu].socket){
+    con.catalog[4].models[col].socket.forEach((item) => {
+      if (item == con.catalog[1].models[cpu].socket)
         cp = 1;
-        break;
-      }
-    }
+    });
     if (cp == 0)
-    console.log('Сокет кулера не совместим спроцессором');
+    str1 = 'Сокет кулера не совместим спроцессором';
     if (con.catalog[4].models[col].power_dissipation < con.catalog[1].models[cpu].tdp)
-    console.log('Мощности кулера недостаточно для отвода тепла, выделяемого процессором');
+    str2 = 'Мощности кулера недостаточно для отвода тепла, выделяемого процессором';
+    if (str1 != "" && str2 != "")
+    return str1 + '\n' + str2;
+    else {
+      if (str1 != "") return str1;
+      if (str2 != "") return str2;
+    }
+  },
+  m2 : function(m2, m) {
+  },
+  case : function (m,c) {
+    if(con.catalog[8].models[c].formfact != con.catalog[0].models[m].formfact)
+    console.log('Корпус и материнская плата несовместимы');
   }
 }
